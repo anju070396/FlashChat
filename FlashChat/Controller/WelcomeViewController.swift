9f0d02e53966573b55f0f0eff5f028deafd657da
Anjali <anjali@flappjacks.com>
2020-05-23 22:39:41 +0530

    Animated Text add.

--- FlashChat/Controller/WelcomeViewController.swift
+++ FlashChat/Controller/WelcomeViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+    
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //typing Animation by CLTypingLabel pod
+        self.titleLabel.text =  "⚡️FlashChat"
+        self.titleLabel.charInterval = 0.04
+        
+        
+        //typing Animation by loop
+//        let title = "⚡️FlashChat"
+//        var charIndex = 0.0
+//        for letter in title  {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+    }
     
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '9.0'
+
+target 'FlashChat' do
+  use_frameworks!
+
+  # Pods for FlashChat
+  
+  pod 'CLTypingLabel'
+
+end
